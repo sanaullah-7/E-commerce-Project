56cea485f55ccce3fdee95d9b64458fe41c92023
sanaullah-7 <sanaullahyousafzai01@gmail.com>
2025-05-27 17:39:58 -0700

    first commit

--- src/components/Subscribe/Subscribe.jsx
+++ src/components/Subscribe/Subscribe.jsx
@@ -4,7 +4,7 @@ const Subscribe = () => {
   return (
     <div
       data-aos="zoom-in"
-      className="mb-20 bg-slate-700 dark:bg-gray-800 text-white "
+      className="mb-20 bg-slate-700 dark:bg-gray-800 text-black "
     >
       <div className="container backdrop-blur-sm py-10">
         <div className="space-y-6 max-w-xl mx-auto">
